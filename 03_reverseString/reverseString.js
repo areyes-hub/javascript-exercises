75856cc0aeaba00ae5f54b0e83c5f1f35ef78df0
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-07-30 10:27:51 -0400

    Update code to pass all test modules

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let result = "";
+    let len = string.length - 1;
+    for (let i = len; i >= 0; i--) {
+        result += string[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
