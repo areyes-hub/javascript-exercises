14525f3f9b51904c7eafad3d8c63f59051c9fc9c
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-07-30 10:06:20 -0400

    Update code to pass all test modules

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,6 +3,9 @@ const repeatString = function (word, numOfRep) {
     for (let i = 0; i < numOfRep; i++) {
         result += word;
     }
+    if (numOfRep < 0) {
+        return "ERROR";
+    }
     return result;
 };
 
