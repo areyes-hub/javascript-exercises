da09ad50916f237ead629a93f5373dfa77e26953
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-07-30 09:17:11 -0400

    Create a function that repeats any input string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (word, numOfRep) {
+    let result = "";
+    for (let i = 0; i < numOfRep; i++) {
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
