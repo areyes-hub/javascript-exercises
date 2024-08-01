66a9935710ae48a1b01ef4bc695aed2aff4e9d5d
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-08-01 08:38:28 -0400

    Create a function that returns the sum of a range of numbers between two given numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+    let sum = 0;
+    let firstNonInt = (firstNum - Math.floor(firstNum)) !== 0;
+    let secondNonInt = (secondNum - Math.floor(secondNum)) !== 0;
 
+    if (typeof firstNum !== "number"
+        || firstNum < 0 || firstNonInt
+        || typeof secondNum !== "number"
+        || secondNonInt || secondNum < 0) {
+        sum = "ERROR";
+    }
+    else if (firstNum < secondNum) {
+        for (let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (let i = secondNum; i <= firstNum; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
