c2adc117563f15b310f79fefe35be0ac7cccb490
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-08-01 00:10:12 -0400

    Create and update function to remove array elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...params) {
+    for (let i = 0; i <= params.length; i++) {
+        for (let j = 0; j <= arr.length; j++) {
+            if (arr[j] === params[i]) {
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
