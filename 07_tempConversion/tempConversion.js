1f52bb71f49d21382640423e98f9aa62e4d84e75
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-08-01 17:32:03 -0400

    create two functions that convert a temperature input from celsius to farenheit and viceversa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+  let celsius = ((farenheit - 32) * 5 / 9).toFixed(1);
+  let isDecimal = (farenheit - Math.floor(farenheit)) !== 0;
+  let wholeCelsius = Math.trunc(+celsius);
+
+  if (isDecimal) {
+    return wholeCelsius;
+  }
+  return +celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let farenheit = (celsius * (9 / 5) + 32).toFixed(1);
+  return +farenheit;
 };
 
 // Do not edit below this line
