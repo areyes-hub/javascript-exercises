635122f4223c40a7c36655373ce709590875747c
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-08-02 09:18:24 -0400

    Create multiple functions to perform addition, subtraction, multiplication, power, sum and factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  let sum = +num1 + +num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  let diff = num1 - num2;
+  return diff;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  else {
+    let result = nums.reduce((num1, num_n) => +num1 + +num_n);
+    return result;
+  }
+
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  let result = nums.reduce((num1, num_n) => num1 * num_n, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  let pow = num1 ** num2;
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let fact = 1;
+  let zero = 0;
+  if (number === zero) {
+    return fact;
+  }
+  else {
+    for (let i = fact; i <= number; i++) {
+      fact *= i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
