b99cc143bc6b36a1075a71c2beb51bbd58df6350
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-08-03 00:28:46 -0400

    Create a function that returns a specific member of the fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (n1) {
+    if (n1 === 0 || n1 === "0") {
+        return 0;
+    }
+    else if (n1 < 0) {
+        return "OOPS";
+    }
+    else {
+        let fib = [1, 1, 2]
+        for (let i = 2; i < n1; i++) {
+            fib.push(+fib[i] + (+fib[i - 1]));
+        }
+        return fib[n1 - 1];
+    }
 };
 
 // Do not edit below this line
