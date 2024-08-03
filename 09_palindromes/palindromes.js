7d11beec428c0b13f43c93b64f85611f7178a9b0
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-08-02 23:49:03 -0400

    Create a function to determine if a word or group of words are palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let reversed = "";
+    let reved = "";
+    let regex = /[!., ]+/g
+    let strLast = string.length - 1;
 
+    for (let i = strLast; i >= 0; i--) {
+        reversed += string[i];
+        reved = reversed.replaceAll(regex, "").toLowerCase();
+    } if (reved === string.replaceAll(regex, "").toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
