cdf8a04a0c542232e90587533523d2afbd0e2fa6
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-08-03 08:47:38 -0400

    Create a function to get the titles of two books

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+    let titles = [];
 
+    titles[0] = books[0].title;
+    titles[1] = books[1].title;
+
+    // Alternate solution:
+
+    // for (let i = 0; i < 2; i++) {
+    //     titles.push(books[i].title)
+    // }
+
+    return titles;
 };
 
 // Do not edit below this line
